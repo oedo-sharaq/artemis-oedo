8b5559fec7cd0592d5c3eadcd0f2d058d2f2eb1a
ShutaroHanai <hanai@cns,s,u-tokyo.ac.jp>
2022-04-10 20:19:48 +0900

    [macro] setalias.C update

--- macro/setalias.C
+++ macro/setalias.C
@@ -1,36 +1,29 @@
 {
 
-
-// gROOT->ProcessLine("tree->SetAlias("sr9.fX","(345*sr91_xc0.fPosition+100*sr92_xc0.fPosition)/(100+345)")");
-// gROOT->ProcessLine("tree->SetAlias("sr9.fY","(345*sr91_yc0.fPosition+100*sr92_yc0.fPosition)/(100+345)")");
-//
-// gROOT->ProcessLine("tree->SetAlias("sr9.fA","445./TMath::Abs(sr91_xc0.fPosition-sr92_xc0.fPosition)")");
-// gROOT->ProcessLine("tree->SetAlias("sr9.fB","445./TMath::Abs(sr91_yc0.fPosition-sr92_yc0.fPosition)")");
-// 
-// gROOT->ProcessLine("tree->SetAlias("tof:","f3pla.fTiming-f2pla.fTiming")");
-//
-//
-//tree->SetAlias("sr9.fX","(345*sr91_xc0.fPosition+100*sr92_xc0.fPosition)/(100+345)")
-//tree->SetAlias("sr9.fY","(345*sr91_yc0.fPosition+100*sr92_yc0.fPosition)/(100+345)")
-//
-//tree->SetAlias("sr9.fA","445./TMath::Abs(sr91_xc0.fPosition-sr92_xc0.fPosition)")
-//tree->SetAlias("sr9.fB","445./TMath::Abs(sr91_yc0.fPosition-sr92_yc0.fPosition)")
-//
-//tree->SetAlias("tof","f3pla.fTiming-f2pla.fTiming")
-
-
 //tree->SetAlias("pidgate","abs(f5track.fX+23.9*((f5ppac1a.fTAnode+f5ppac2a.fTAnode)/2-diapad.fTiming-1250)-1414-32369)<9")
 //tree->SetAlias("pidgate","abs(f5track.fX+25.5*((f5ppac1a.fTAnode+f5ppac2a.fTAnode)/2-diapad.fTiming-1250)-36040)<10")
 tree->SetAlias("pidgate","abs(f5track.fX+26.6*((f5ppac1a.fTAnode+f5ppac2a.fTAnode)/2-diapad.fTiming-1250)-37595)<10")
-//tree->SetAlias("sr9x","(542.25*sr91_x.fPosition-122.5*sr92_x.fPosition)/419.75")
-//tree->SetAlias("sr9a","(sr91_x.fPosition-sr92_x.fPosition)/419.75")
-//tree->SetAlias("sr9y","(542.25*sr91_y.fPosition-122.5*sr92_y.fPosition)/419.75")
-//tree->SetAlias("sr9b","(sr91_y.fPosition-sr92_y.fPosition)/419.75")
+
+//tree->SetAlias("sr9.fX","(542.25*sr91_x.fPosition-122.5*sr92_x.fPosition)/419.75")
+//tree->SetAlias("sr9.fA","(sr91_x.fPosition-sr92_x.fPosition)/419.75")
+//tree->SetAlias("sr9.fY","(542.25*sr91_y.fPosition-122.5*sr92_y.fPosition)/419.75")
+//tree->SetAlias("sr9.fB","(sr91_y.fPosition-sr92_y.fPosition)/419.75")
 //
-//tree->SetAlias("srcx","(1972*src2_x.fPosition-1452*src1_x.fPosition)/520")
-//tree->SetAlias("srca","(src2_x.fPosition-src1_x.fPosition)/520")
-//tree->SetAlias("srcy","(1972*src2_y.fPosition-1452*src1_y.fPosition)/520")
-//tree->SetAlias("srcb","(src2_y.fPosition-src1_y.fPosition)/520")
+//tree->SetAlias("src.fX","(1972*src2_x.fPosition-1452*src1_x.fPosition)/520.")
+//tree->SetAlias("src.fA","(src1_x.fPosition-src2_x.fPosition)/520.")
+//tree->SetAlias("src.fY","(1972*src2_y.fPosition-1452*src1_y.fPosition)/520.")
+//tree->SetAlias("src.fB","(src1_y.fPosition-src2_y.fPosition)/520")
+
+//tree->SetAlias("sr1.fX","(560.8*sr12_x.fPosition-210.8*sr11_x.fPosition)/350.")
+//tree->SetAlias("sr1.fA","(sr11_x.fPosition-sr12_x.fPosition)/350.")
+//tree->SetAlias("sr1.fY","(560.8*sr12_y.fPosition-210.8*sr11_y.fPosition)/350.")
+//tree->SetAlias("sr1.fB","(sr11_y.fPosition-sr12_y.fPosition)/350.")
+
+
+tree->SetAlias("tof3c","(src1_a_raw.fTiming+src2_a_raw.fTiming)/2-f3la.fTiming")
+tree->SetAlias("tof39","(sr91_a_raw.fTiming+sr92_a_raw.fTiming)/2-f3pla.fTiming")
+tree->SetAlias("tof9c","(src1_a_raw.fTiming+src2_a_raw.fTiming)/2-(sr91_a_raw.fTiming+sr92_a_raw.fTiming)/2")
+tree->SetAlias("tof9s","(sr11_a_raw.fTiming+sr12_a_raw.fTiming)/2-(sr91_a_raw.fTiming+sr92_a_raw.fTiming)/2")
 
 }
 
