918cb7240671d91128f2e1f888e423a5686e95b0
taiga haginouchi <taigah.1216@gmail.com>
2022-06-29 13:39:22 +0900

    for TiNA calib and search missing ID macros

---
+++ macro/missingID.cc
@@ -0,0 +1,19 @@
+void missingID(Int_t num, Int_t get)
+{
+	{
+		TString cmd;
+		cmd.Form("add steering/missingID.yaml NAME=physics NUM=%04d GET=%04d",num,get);
+		gROOT->ProcessLine(cmd.Data());
+		art::TLoopManager::Instance()->GetLoop()->Resume();
+	}
+
+	cout << num << endl;
+	cout << endl;
+	gROOT->ProcessLine(".x macro/search_TiNA_ID.cc");
+//	gROOT->ProcessLine("cd");
+//	gROOT->ProcessLine("zone");
+//	gROOT->ProcessLine("ht missing_ID/csiraw_id_q colz");
+//	gROOT->ProcessLine("lgy");
+//	gROOT->ProcessLine("lgz");
+//	gROOT->ProcessLine("csiraw_id_q->SetStats(0)");
+}
