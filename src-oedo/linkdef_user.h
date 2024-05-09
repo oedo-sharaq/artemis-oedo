b97e27214afc7ab4c5dda965e72051a19d9bb2e6
ShutaroHanai <hanai@cns.s.u-tokyo.ac.jp>
2024-05-09 17:35:15 +0900

    new proc srppac

--- src-oedo/linkdef_user.h
+++ src-oedo/linkdef_user.h
@@ -44,6 +44,9 @@
 #pragma link C++ class art::TBrhoReconstructProcessorS1;
 #pragma link C++ class art::TPIDSingleSectionProcessor;
 #pragma link C++ class art::TCmdPadZoom;
+#pragma link C++ class art::TExpConverter;
+#pragma link C++ class art::TSRPPACPlaneQ1Q2Q0Q2Processor;
+#pragma link C++ class art::TSRPPACPlaneGeneralizedRatioProcessor;
 #endif // __CINT__
 
 #endif // LINKDEF_USER_H
